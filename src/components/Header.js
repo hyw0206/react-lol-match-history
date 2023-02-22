f4cdc3364030752a712af4dc2f0bb23cf177314c
hyw0206 <114200503+hyw0206@users.noreply.github.com>
2023-02-22 15:57:40 +0900

    Update Header.js

--- src/components/Header.js
+++ src/components/Header.js
@@ -6,8 +6,6 @@ const Header = () => {
       <a rel="noopener noreferrer" target="_blank" href="https://www.leagueoflegends.com/ko-kr/"><div className="nav-item">Game Homepage</div></a>
       <a rel="noopener noreferrer" target="_blank" href="https://github.com/hyw0206/react-lol-match-history"><div className="nav-item">Source Code</div></a>
       <div className="nav-item">|</div>
-      <a href="#match"><div className="nav-item">게임 전적</div></a>
-      <a href="#mastery"><div className="nav-item">숙련도 정보</div></a>
     </header>
   )
 }
